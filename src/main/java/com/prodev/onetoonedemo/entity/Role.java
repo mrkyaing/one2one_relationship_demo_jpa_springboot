daf1a8f903be6482a76ce6e5781bc1061ad0b4c0
Phyo Ei San <phyoeisann12345@gmail.com>
2020-08-28 23:32:13 -0700

    UserInRole

--- src/main/java/com/prodev/onetoonedemo/entity/Role.java
+++ src/main/java/com/prodev/onetoonedemo/entity/Role.java
@@ -4,4 +4,25 @@ public class Role {
 private int Id;
 private String name;
 private int roleLevel;
+public int getId() {
+	return Id;
 }
+public void setId(int id) {
+	Id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getRoleLevel() {
+	return roleLevel;
+}
+public void setRoleLevel(int roleLevel) {
+	this.roleLevel = roleLevel;
+}
+
+
+}
+	
---
+++ src/main/java/com/prodev/onetoonedemo/entity/UserInRole.java
@@ -0,0 +1,9 @@
+package com.prodev.onetoonedemo.entity;
+
+public class UserInRole {
+
+	public UserInRole() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
